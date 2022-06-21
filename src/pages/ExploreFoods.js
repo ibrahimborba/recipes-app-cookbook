52d779c92f6e75c787d6ac459d5c71f4673caeee
Victor Vidal <victorvidal08@hotmail.com>
2022-06-21 16:09:23 -0300

    feat: Explore page buttons and routes

--- src/pages/ExploreFoods.js
+++ src/pages/ExploreFoods.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function ExploreFoods() {
+  const history = useHistory();
   return (
     <>
       <Header />
+      <form>
+        <button
+          type="button"
+          data-testid="explore-by-ingredient"
+          onClick={ () => history.push('/explore/foods/ingredients') }
+        >
+          By Ingredient
+        </button>
+        <button
+          type="button"
+          data-testid="explore-by-nationality"
+          onClick={ () => history.push('/explore/foods/nationalities') }
+        >
+          By Nationality
+        </button>
+        <button
+          type="button"
+          data-testid="explore-surprise"
+          onClick={ () => console.log('bebidas aleatórias') }
+        >
+          Surprise me
+        </button>
+      </form>
       <Footer />
     </>
   );
