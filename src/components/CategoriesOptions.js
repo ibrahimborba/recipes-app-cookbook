23eb44592552d0a9d57beefd3b2536555db27442
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-06-21 18:27:05 -0300

    feat: add api request to categories

--- src/components/CategoriesOptions.js
+++ src/components/CategoriesOptions.js
@@ -1,20 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { getCategories } from '../services/api';
 
-function Foods() {
-  const [categories] = useState([]);
+function CategoriesOptions() {
+  const [categories, setCategories] = useState([]);
+  const { pathname } = useLocation();
+
+  const MAX_ITEMS_DISPLAY = 5;
+
+  useEffect(() => {
+    const categoriesList = async () => {
+      const categoriesResult = await getCategories(pathname);
+      setCategories(categoriesResult);
+    };
+    categoriesList();
+  }, []);
 
   return (
     <section>
-      { categories.map((category) => (
+      { categories.slice(0, MAX_ITEMS_DISPLAY).map((category) => (
         <button
-          key={ category }
+          key={ category.strCategory }
+          data-testid={ `${category.strCategory}-category-filter` }
           type="button"
         >
-          {category}
+          {category.strCategory}
         </button>
       ))}
     </section>
   );
 }
 
-export default Foods;
+export default CategoriesOptions;
--- src/services/api.js
+++ src/services/api.js
@@ -15,7 +15,6 @@ export const getMeal = async (search, option) => {
   }
 
   try {
-    console.log(`${URL}${search}`);
     const apiResponse = await fetch(`${URL}${search}`);
     const apiResult = await apiResponse.json();
     return apiResult;
@@ -44,6 +43,32 @@ export const getDrink = async (search, option) => {
     const apiResponse = await fetch(`${URL}${search}`);
     const apiResult = await apiResponse.json();
     return apiResult;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const getCategories = async (pathname) => {
+  let URL = '';
+  let key = '';
+  switch (pathname) {
+  case '/foods':
+    URL = 'https://www.themealdb.com/api/json/v1/1/list.php?c=list';
+    key = 'meals';
+    break;
+  case '/drinks':
+    URL = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
+    key = 'drinks';
+    break;
+  default:
+    break;
+  }
+
+  try {
+    const apiResponse = await fetch(URL);
+    const apiResult = await apiResponse.json();
+    const categoriesList = apiResult[key];
+    return categoriesList;
   } catch (error) {
     return error;
   }
