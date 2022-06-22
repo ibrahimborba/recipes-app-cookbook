f48e24c2c5909bf038e7e3f31350e89fcc041ec1
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-06-22 16:22:02 -0300

    feat: click on ingredient redirect to main page and filter by clicked ingredient

--- src/pages/FoodsIngredients.js
+++ src/pages/FoodsIngredients.js
@@ -1,13 +1,15 @@
 import React, { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useHistory } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import { fetchMealsIngredientsResults } from '../redux/actions';
+import { fetchMealsIngredientsResults, searchOptions } from '../redux/actions';
 
 function FoodsIngredients() {
   const dispatch = useDispatch();
   const { pathname } = useLocation();
+  const history = useHistory();
+
   const MAX_INGREDIENTS_DISPLAY = 12;
   const mealIngredientsResults = useSelector((state) => state.ingredientsResults.meals);
 
@@ -18,21 +20,32 @@ function FoodsIngredients() {
     fetchData();
   }, [pathname, dispatch]);
 
+  const handleClick = (value) => () => {
+    dispatch(searchOptions(value, 'ingredient'));
+    history.push('/foods');
+  };
+
   return (
     <>
       <Header />
       {mealIngredientsResults.slice(0, MAX_INGREDIENTS_DISPLAY).map((ing, index) => (
-        <div
+        <button
           key={ ing.idIngredient }
           data-testid={ `${index}-ingredient-card` }
+          type="button"
+          onClick={ handleClick(ing.strIngredient) }
         >
           <img
             src={ `https://www.themealdb.com/images/ingredients/${ing.strIngredient}-Small.png` }
             alt={ ing.strIngredient }
             data-testid={ `${index}-card-img` }
           />
-          <p data-testid={ `${index}-card-name` }>{ ing.strIngredient }</p>
-        </div>
+          <p
+            data-testid={ `${index}-card-name` }
+          >
+            { ing.strIngredient }
+          </p>
+        </button>
       ))}
       <Footer />
     </>
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,7 +2,13 @@ import { combineReducers } from 'redux';
 import searchResults from './searchResultReducer';
 import randomResults from './randomReducer';
 import ingredientsResults from './ingredientsReducer';
+import searchOptions from './searchOptionsReducer';
 
-const rootReducer = combineReducers({ searchResults, randomResults, ingredientsResults });
+const rootReducer = combineReducers({
+  searchResults,
+  randomResults,
+  ingredientsResults,
+  searchOptions,
+});
 
 export default rootReducer;
