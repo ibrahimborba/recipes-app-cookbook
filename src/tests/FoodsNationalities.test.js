6c1301e28c3e0034c60b206c80140a16a94618c6
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-06-24 17:13:48 -0300

    feat: add tests to FoodsNationalities page" "

---
+++ src/tests/FoodsNationalities.test.js
@@ -0,0 +1,142 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouterRedux from './helpers/renderWithRouterRedux';
+import meals from './mocks/meals';
+import nationalities from './mocks/nationalitiesMeal';
+import filtered from './mocks/americanMeals';
+
+const SEARCH_ICON = 'search icon';
+const PATH = '/explore/foods/nationalities';
+
+describe('1 - Foods Nationalities page, Header component tests', () => {
+  it('checks if Header is rendered and shows SearchBar when search icon is clicked',
+    async () => {
+      renderWithRouterRedux(<App />, {
+        initialEntries: [PATH],
+      });
+
+      const pageTitle = screen.getByRole('heading',
+        { name: 'Explore Nationalities', level: 1 });
+      const profileImg = screen.getByRole('img', { name: 'profile icon' });
+      const searchBtn = screen.getByRole('img', { name: SEARCH_ICON });
+      const searchInputDisabled = screen.queryByLabelText('Search');
+
+      expect(pageTitle).toBeInTheDocument();
+      expect(profileImg).toBeInTheDocument();
+      expect(searchBtn).toBeInTheDocument();
+      expect(searchInputDisabled).not.toBeInTheDocument();
+
+      userEvent.click(searchBtn);
+
+      const searchInputEnabled = await screen.findByLabelText('Search');
+      expect(searchInputEnabled).toBeInTheDocument();
+    });
+});
+
+describe('2 - Foods Nationalities page, Nationalities component tests', () => {
+  beforeEach(() => {
+    jest.spyOn(global, 'fetch')
+      .mockImplementation(() => Promise.resolve({
+        json: () => Promise.resolve(nationalities),
+      }));
+  });
+
+  afterEach(() => jest.restoreAllMocks());
+
+  it('checks if Nationalities options are rendered as expected',
+    async () => {
+      const NATIONALITIES_OPTIONS = 28;
+
+      renderWithRouterRedux(<App />, {
+        initialEntries: [PATH],
+      });
+
+      const americanOptionBtn = await screen.findByRole('option', { name: 'American' });
+      const categoriesBtns = screen.getAllByRole('option');
+      expect(americanOptionBtn).toBeInTheDocument();
+      expect(categoriesBtns).toHaveLength(NATIONALITIES_OPTIONS);
+    });
+
+  it('checks if when American option is clicked recipes by American nationality',
+    async () => {
+      const CARDS_LENGTH = 12;
+
+      renderWithRouterRedux(<App />, {
+        initialEntries: [PATH],
+      });
+
+      const americanOptionBtn = await screen
+        .findByRole('option', { name: 'American' });
+      expect(americanOptionBtn).toBeInTheDocument();
+
+      jest.spyOn(global, 'fetch')
+        .mockImplementation(() => Promise.resolve({
+          json: () => Promise.resolve(filtered),
+        }));
+
+      userEvent.click(americanOptionBtn);
+
+      expect(global.fetch).toBeCalledWith('https://www.themealdb.com/api/json/v1/1/filter.php?a=American');
+
+      const images = await screen.findAllByRole('img');
+      const recipesCards = images.filter((img) => !img.alt.includes('icon'));
+      expect(recipesCards).toHaveLength(CARDS_LENGTH);
+    });
+
+  it('checks if All Nationalities resets filter', async () => {
+    renderWithRouterRedux(<App />, {
+      initialEntries: [PATH],
+    });
+
+    expect(global.fetch).toHaveBeenCalledTimes(2);
+
+    const beefCategoryBtn = await screen.findByRole('option', { name: 'All' });
+    expect(beefCategoryBtn).toBeInTheDocument();
+
+    jest.spyOn(global, 'fetch')
+      .mockImplementation(() => Promise.resolve({
+        json: () => Promise.resolve(meals),
+      }));
+
+    userEvent.click(beefCategoryBtn);
+  });
+});
+
+describe('3 - Foods Nationalities page, Footer component tests', () => {
+  it('checks if Footer is rendered with drink, explore and meal icons',
+    async () => {
+      const { history } = renderWithRouterRedux(<App />, { initialEntries: [PATH] });
+
+      const drinksBtnFooter = screen.getByRole('button', { name: 'drink-icon' });
+      const exploreBtnFooter = screen.getByRole('button', { name: 'explore-icon' });
+      const mealBtnFooter = screen.getByRole('button', { name: 'meal-icon' });
+
+      expect(drinksBtnFooter).toBeInTheDocument();
+      expect(exploreBtnFooter).toBeInTheDocument();
+      expect(mealBtnFooter).toBeInTheDocument();
+
+      userEvent.click(drinksBtnFooter);
+      expect(history.location.pathname).toBe('/drinks');
+    });
+
+  it('checks if when Drink, Explore and Food icons are clicked, the path changes',
+    async () => {
+      const { history } = renderWithRouterRedux(<App />, {
+        initialEntries: [PATH],
+      });
+
+      const drinksBtnFooter = screen.getByRole('button', { name: 'drink-icon' });
+      userEvent.click(drinksBtnFooter);
+      expect(history.location.pathname).toBe('/drinks');
+
+      const exploreBtnFooter = screen.getByRole('button', { name: 'explore-icon' });
+      userEvent.click(exploreBtnFooter);
+      expect(history.location.pathname).toBe('/explore');
+
+      const mealBtnFooter = screen.getByRole('button', { name: 'meal-icon' });
+      userEvent.click(mealBtnFooter);
+      expect(history.location.pathname).toBe('/foods');
+    });
+});
---
+++ src/tests/mocks/americanMeals.js
@@ -0,0 +1,71 @@
+const americanMeals = {
+  meals: [
+    {
+      strMeal: 'Banana Pancakes',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/sywswr1511383814.jpg',
+      idMeal: '52855',
+    },
+    {
+      strMeal: 'BBQ Pork Sloppy Joes',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/atd5sh1583188467.jpg',
+      idMeal: '52995',
+    },
+    {
+      strMeal: 'Beef Brisket Pot Roast',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/ursuup1487348423.jpg',
+      idMeal: '52812',
+    },
+    {
+      strMeal: 'Big Mac',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/urzj1d1587670726.jpg',
+      idMeal: '53013',
+    },
+    {
+      strMeal: 'Chick-Fil-A Sandwich',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/sbx7n71587673021.jpg',
+      idMeal: '53016',
+    },
+    {
+      strMeal: 'Chicken Fajita Mac and Cheese',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/qrqywr1503066605.jpg',
+      idMeal: '52818',
+    },
+    {
+      strMeal: 'Choc Chip Pecan Pie',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/rqvwxt1511384809.jpg',
+      idMeal: '52856',
+    },
+    {
+      strMeal: 'Chocolate Raspberry Brownies',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/yypvst1511386427.jpg',
+      idMeal: '52860',
+    },
+    {
+      strMeal: 'Clam chowder',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/rvtvuw1511190488.jpg',
+      idMeal: '52840',
+    },
+    {
+      strMeal: 'French Onion Chicken with Roasted Carrots & Mashed Potatoes',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/b5ft861583188991.jpg',
+      idMeal: '52996',
+    },
+    {
+      strMeal: 'Fruit and Cream Cheese Breakfast Pastries',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/1543774956.jpg',
+      idMeal: '52957',
+    },
+    {
+      strMeal: 'Grilled Mac and Cheese Sandwich',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/xutquv1505330523.jpg',
+      idMeal: '52829',
+    },
+    {
+      strMeal: 'Honey Balsamic Chicken with Crispy Broccoli & Potatoes',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/kvbotn1581012881.jpg',
+      idMeal: '52993',
+    },
+  ],
+};
+
+export default americanMeals;
