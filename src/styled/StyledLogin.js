a60373a2fa8745951f4aee2c0d691835f07be451
Victor Vidal <victorvidal08@hotmail.com>
2022-06-28 19:24:14 -0300

    feat: adding login page style

---
+++ src/styled/StyledLogin.js
@@ -0,0 +1,67 @@
+import styled from 'styled-components';
+
+export const StyledLogin = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items:center;
+  flex-direction:column;
+  background-image: url("https://images.unsplash.com/photo-1529543544282-ea669407fca3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80");
+  background-size: cover;
+  background-position: right;
+
+  h1 {
+    margin-top: 55%;
+    margin-bottom: 20px;
+    font-size: 2.5rem;
+    color: ${(props) => props.theme.mainColor};
+    font-weight: 600;
+    
+  }
+`;
+
+export const StyledCard = styled.div`
+  padding: 16px;
+  background: rgba(74, 74, 74, 0.25);
+  border-radius: 16px;
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(5px);
+  -webkit-backdrop-filter: blur(5px);
+  border: 1px solid rgba(255, 255, 255, 0.18);
+
+  input {
+    margin-top: 3px;
+    display: flex;
+    align-items: center;
+    margin-bottom: 15px;
+    border: none;
+    border-bottom: 1px solid ${(props) => props.theme.mainColor};
+    background: transparent;
+    ::placeholder {
+      color: ${(props) => props.theme.mainColor};
+      font-style: italic;
+      font-size: 0.65rem;
+    }
+    :focus {
+      color: ${(props) => props.theme.mainColor};
+    }
+  }
+
+  label {
+    color: ${(props) => props.theme.mainColor};
+    font-weight: 600;
+    font-size: 0.8rem;
+  }
+
+  button {
+    margin: 0 auto;
+    height: 1rem;
+    background-color: ${(props) => props.theme.secondaryColor};
+    color: ${(props) => props.theme.mainColor};
+    padding: 10px;
+    border-radius: 5px;
+    font-weight: 600;
+    font-size: 0.8rem;
+    display: flex;
+    align-items: center;
+  }
+`;
