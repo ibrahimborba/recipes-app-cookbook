2d2ecd8c961ee9d7372796385efba6f236d51c79
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-06-29 17:57:06 -0300

    feat: add style to explore ingredients foods and drinks

--- src/pages/DrinksIngredients.js
+++ src/pages/DrinksIngredients.js
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { fetchDrinksIngredientsResults, searchOptions } from '../redux/actions';
+import StyledIngredients from '../styled/StyledIngredients';
 
 function DrinksIngredients() {
   const dispatch = useDispatch();
@@ -29,21 +30,23 @@ function DrinksIngredients() {
   return (
     <>
       <Header />
-      {drinksIngredientsResults.slice(0, MAX_INGREDIENTS_DISPLAY).map((ing, index) => (
-        <button
-          key={ ing.strIngredient1 }
-          data-testid={ `${index}-ingredient-card` }
-          type="button"
-          onClick={ handleClick(ing.strIngredient1) }
-        >
-          <img
-            src={ `https://www.thecocktaildb.com/images/ingredients/${ing.strIngredient1}-Small.png` }
-            alt={ ing.strIngredient1 }
-            data-testid={ `${index}-card-img` }
-          />
-          <p data-testid={ `${index}-card-name` }>{ ing.strIngredient1 }</p>
-        </button>
-      ))}
+      <StyledIngredients>
+        {drinksIngredientsResults.slice(0, MAX_INGREDIENTS_DISPLAY).map((ing, index) => (
+          <button
+            key={ ing.strIngredient1 }
+            data-testid={ `${index}-ingredient-card` }
+            type="button"
+            onClick={ handleClick(ing.strIngredient1) }
+          >
+            <img
+              src={ `https://www.thecocktaildb.com/images/ingredients/${ing.strIngredient1}-Small.png` }
+              alt={ ing.strIngredient1 }
+              data-testid={ `${index}-card-img` }
+            />
+            <p data-testid={ `${index}-card-name` }>{ ing.strIngredient1 }</p>
+          </button>
+        ))}
+      </StyledIngredients>
       <Footer />
     </>
   );
