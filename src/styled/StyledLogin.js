78e5e6bd850a917482eea10305f3d8a5162186f5
samuel santos <samaraujo28@gmail.com>
2022-06-29 23:42:15 -0300

    style: add disabled style in login page

--- src/styled/StyledLogin.js
+++ src/styled/StyledLogin.js
@@ -1,67 +1,93 @@
 import styled from 'styled-components';
+import bgCover from '../images/wine_background.png';
 
 export const StyledLogin = styled.div`
-  height: 100vh;
-  display: flex;
   align-items:center;
-  flex-direction:column;
-  background-image: url("./wine_background.jpg");
-  background-size: cover;
+  background-image: url(${bgCover});
   background-position: right;
+  background-size: cover;
+  display: flex;
+  flex-direction:column;
+  height: 100vh;
+  justify-content: center;
 
   h1 {
-    margin-top: 55%;
-    margin-bottom: 20px;
-    font-size: 2.5rem;
     color: ${(props) => props.theme.mainColor};
+    font-size: 2.5rem;
     font-weight: 600;
-    
+    margin-bottom: 20px;
+    text-shadow: 6px 6px 7px rgba(0,0,0,0.8);
   }
 `;
 
 export const StyledCard = styled.div`
-  padding: 16px;
-  background: rgba(74, 74, 74, 0.25);
-  border-radius: 16px;
-  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
   backdrop-filter: blur(5px);
-  -webkit-backdrop-filter: blur(5px);
+  background: rgba(74, 74, 74, 0.6);
   border: 1px solid rgba(255, 255, 255, 0.18);
+  border-radius: 16px;
+  box-shadow: 0 5px 30px rgba(0, 0, 0, 0.4);
+  padding: 16px;
+  width: 80%;
+  max-width: 350px;
+  display: flex;
+  justify-content: center;
+
+  -webkit-backdrop-filter: blur(5px);
+
+  form {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    margin-top: 1rem;
+    width: 90%;
+  }
 
   input {
-    margin-top: 3px;
-    display: flex;
-    align-items: center;
-    margin-bottom: 15px;
+    background: transparent;
     border: none;
     border-bottom: 1px solid ${(props) => props.theme.mainColor};
-    background: transparent;
+    color: ${(props) => props.theme.mainColor};
+    margin-top: .5rem;
+    margin-bottom: 1.5rem;
+    width: 100%;
+    font-size: 1rem;
+
     ::placeholder {
-      color: ${(props) => props.theme.mainColor};
+      color: ${(props) => props.theme.lightgray};
+      font-size: 0.9rem;
       font-style: italic;
-      font-size: 0.65rem;
+      font-weight: 500;
     }
+
     :focus {
       color: ${(props) => props.theme.mainColor};
+      outline: none;
     }
   }
 
   label {
     color: ${(props) => props.theme.mainColor};
-    font-weight: 600;
-    font-size: 0.8rem;
+    font-size: 1.2rem;
+    font-weight: 700;
+    max-width: 300px;
   }
 
   button {
-    margin: 0 auto;
-    height: 1rem;
-    background-color: ${(props) => props.theme.secondaryColor};
-    color: ${(props) => props.theme.mainColor};
-    padding: 10px;
+    background-color: ${(props) => props.theme.dark};
+    border: none;
     border-radius: 5px;
+    color: ${(props) => props.theme.mainColor};
+    font-size: 1rem;
     font-weight: 600;
-    font-size: 0.8rem;
-    display: flex;
-    align-items: center;
+    margin: 1rem 0;
+    padding: .5rem 1rem;
+    transition: 0.4s;
+    width: 50%;
+  }
+
+  button:disabled {
+    background-color: ${({ theme }) => theme.disabled};
+    color: ${({ theme }) => theme.secondary};
+    transition: 0.4s;
   }
 `;
